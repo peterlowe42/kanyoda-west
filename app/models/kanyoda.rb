84f19910a15475d0f031e6f74ceaac14fa628570
peterlowe42 <peter.lowe42@gmail.com>
2016-04-12 13:38:09 -0700

    Add yodaize method

--- app/models/kanyoda.rb
+++ app/models/kanyoda.rb
@@ -1,8 +1,22 @@
 class Kanyoda < ActiveRecord::Base
+  include ApplicationHelper
 
-  def self.get_tweet
+  def self.get_tweet(rand=nil)
     tweet = $twitter.user_timeline("kanyewest").first.text
     return tweet 
   end
+
+  def self.yodaize(tweet)
+    split_tweet = tweet.split(' ')
+    sentence = split_tweet.join('+')
+    url = "https://yoda.p.mashape.com/yoda?sentence=" + sentence
+    yoda_tweet = Unirest.get url, headers:{
+      "X-Mashape-Key" => "gYgNoRwAphmshSspZHwOnOXJteTSp1sEn1JjsnHBnhXml6bCNu",
+      "Accept" => "text/plain"
+    }
+    return yoda_tweet
+  end
+
+
 
 end
