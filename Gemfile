422059298423d999d3553acb3c7fc24b28584607
peterlowe42 <peter.lowe42@gmail.com>
2016-04-13 11:58:07 -0700

    Change yoda api

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'twitter'
 
 gem 'unirest'
 
+gem 'httparty'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/kanyoda.rb
+++ app/models/kanyoda.rb
@@ -1,22 +1,27 @@
+require 'unirest'
+
 class Kanyoda < ActiveRecord::Base
   include ApplicationHelper
 
   def self.get_tweet(rand=nil)
-    tweet = $twitter.user_timeline("kanyewest").first.text
+    if rand.nil?
+      tweet = $twitter.user_timeline("kanyewest").first.text
+    else
+      tweet = $twitter.user_timeline("kanyewest")[rand].text
+    end
     return tweet 
   end
 
+
   def self.yodaize(tweet)
-    split_tweet = tweet.split(' ')
-    sentence = split_tweet.join('+')
-    url = "https://yoda.p.mashape.com/yoda?sentence=" + sentence
-    yoda_tweet = Unirest.get url, headers:{
-      "X-Mashape-Key" => "gYgNoRwAphmshSspZHwOnOXJteTSp1sEn1JjsnHBnhXml6bCNu",
-      "Accept" => "text/plain"
-    }
-    return yoda_tweet
+    new_string = tweet.dup
+    sentence = new_string.gsub! /\s+/, '%20'
+    p sentence
+    resp = HTTParty.get("http://api.funtranslations.com/translate/yoda.json?text=#{sentence}.", headers:{ 'X-FunTranslations-Api-Secret' => ENV['FUNTRANSLATION_KEY']})
+    return resp
   end
 
 
 
 end
+
