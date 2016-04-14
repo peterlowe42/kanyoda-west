69ab86fbaeb04e2746b83271ac99517edece043d
peterlowe42 <peter.lowe42@gmail.com>
2016-04-14 11:58:11 -0700

    Remove p statements from twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,11 +1,7 @@
 $twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['CONSUMER_KEY']
-  p "consumer_key: #{config.consumer_key}"
   config.consumer_secret = ENV['CONSUMER_SECRET']
-  p "consumer_secret: #{config.consumer_secret}"
   config.access_token = ENV['ACCESS_TOKEN']
-  p "access_token: #{config.access_token}"
   config.access_token_secret = ENV['ACCESS_SECRET']
-  p "access_token_secret #{config.access_token_secret}"
 end
 
